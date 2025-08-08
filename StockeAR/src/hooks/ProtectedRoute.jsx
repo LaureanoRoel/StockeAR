38032bf78e2485708a460c5c85a125be5da14174
LaureanoRoel <roellaureano@gmail.com>
2025-08-08 15:41:10 -0300

    arreglo de routes

--- StockeAR/src/hooks/ProtectedRoute.jsx
+++ StockeAR/src/hooks/ProtectedRoute.jsx
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+export const ProtectedRoute = ({ user, redirectTo = '/login', children }) => {
+  if (!user) {
+    // Si no hay un objeto 'user', redirige a la página de login
+    return <Navigate to={redirectTo} replace />;
+  }
+
+  // Si hay un objeto 'user', muestra el contenido de la ruta anidada (usando <Outlet />)
+  return children ? children : <Outlet />;
+};
--- StockeAR/src/routers/routes.jsx
+++ StockeAR/src/routers/routes.jsx
@@ -1,32 +1,28 @@
-// routers/routes.jsx (CORREGIDO)
 import { Routes, Route } from "react-router-dom";
+import { ProtectedRoute } from "../hooks/ProtectedRoute"; 
 import {
   Configuracion,
   Home,
   Login,
   Marca,
-  ProtectedRoute,
   UserAuth,
   Categorias,
   Productos,
 } from "../index";
 
 export function MyRoutes() {
-  // Solo necesitamos obtener el estado del usuario para pasarlo a ProtectedRoute
   const { user } = UserAuth();
 
   return (
     <Routes>
-      {/* Esta ruta es pública */}
       <Route path="/login" element={<Login />} />
 
-      {/* Todo lo que esté dentro de este Route estará protegido */}
       <Route element={<ProtectedRoute user={user} redirectTo="/login" />}>
         <Route path="/" element={<Home />} />
         <Route path="/configurar" element={<Configuracion />} />
         <Route path="/configurar/marca" element={<Marca />} />
         <Route path="/configurar/categorias" element={<Categorias />} />
-         <Route path="/productos" element={<Productos />} />
+        <Route path="/productos" element={<Productos />} />
       </Route>
     </Routes>
   );
